74882525acb01a0cddbeda0a4c2bd078976f5d01
Shane Sobus <ssobus@gmail.com>
2020-11-03 09:45:20 -0800

    Added Optimus nad Predaking to automatic import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,9 @@
+INSERT INTO Transformer (id, name, type, strength, intelligence, endurance, speed, rank, courage, firepower) values (HIBERNATE_SEQUENCE.nextval, 'Optimus Prime', 0, 9, 8, 10, 9, 9, 10, 8);
 INSERT INTO Transformer (id, name, type, strength, intelligence, endurance, speed, rank, courage, firepower) values (HIBERNATE_SEQUENCE.nextval, 'Ironhide', 0, 7, 5, 6, 8, 2, 7, 8);
 INSERT INTO Transformer (id, name, type, strength, intelligence, endurance, speed, rank, courage, firepower) values (HIBERNATE_SEQUENCE.nextval, 'Jolt', 0, 5, 3, 4, 4, 4, 3, 5);
 INSERT INTO Transformer (id, name, type, strength, intelligence, endurance, speed, rank, courage, firepower) values (HIBERNATE_SEQUENCE.nextval, 'Brains', 0, 3, 10, 8, 6, 4, 5, 2);
 
+INSERT INTO Transformer (id, name, type, strength, intelligence, endurance, speed, rank, courage, firepower) values (HIBERNATE_SEQUENCE.nextval, 'Predaking', 1, 10, 8, 8, 9, 10, 8, 10);
 INSERT INTO Transformer (id, name, type, strength, intelligence, endurance, speed, rank, courage, firepower) values (HIBERNATE_SEQUENCE.nextval, 'Payload', 1, 7, 2, 5, 5, 4, 9, 8);
 INSERT INTO Transformer (id, name, type, strength, intelligence, endurance, speed, rank, courage, firepower) values (HIBERNATE_SEQUENCE.nextval, 'Soundwave', 1, 5, 4, 7, 10, 8, 7, 5);
 INSERT INTO Transformer (id, name, type, strength, intelligence, endurance, speed, rank, courage, firepower) values (HIBERNATE_SEQUENCE.nextval, 'Bonecrusher', 1, 10, 3, 8, 3, 6, 8, 7);
